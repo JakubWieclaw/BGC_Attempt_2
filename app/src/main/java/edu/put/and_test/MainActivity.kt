6c80ad8c9fd8dd227d0e32a956617516afd45d56
JakubWieclaw <jwieclaw000@gmail.com>
2023-06-08 23:04:20 +0200

    add login user v2

--- app/src/main/java/edu/put/and_test/MainActivity.kt
+++ app/src/main/java/edu/put/and_test/MainActivity.kt
@@ -1,4 +1,4 @@
-package edu.put.BGC_151765
+package edu.put.and_test
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
@@ -8,7 +8,6 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import java.io.BufferedReader
-import java.io.IOException
 import java.io.InputStreamReader
 import java.net.HttpURLConnection
 import java.net.URL
@@ -17,8 +16,6 @@ import java.net.URL
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
-import edu.put.and_test.R
-import edu.put.and_test.UserView
 import java.io.File
 import java.io.FileOutputStream
 import java.io.InputStream
@@ -45,29 +42,6 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-//    private fun performGameCollectionRequest(username: String): String {
-//        val apiUrl = "https://www.boardgamegeek.com/xmlapi2/collection?username=$username"
-//        val url = URL(apiUrl)
-//        val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
-//        connection.requestMethod = "GET"
-//
-//        val responseCode = connection.responseCode
-//
-//        return if (responseCode == HttpURLConnection.HTTP_OK) {
-//            val reader = BufferedReader(InputStreamReader(connection.inputStream))
-//            val response = StringBuilder()
-//            var line: String?
-//            while (reader.readLine().also { line = it } != null) {
-//                response.append(line)
-//            }
-//            reader.close()
-//
-//            response.toString()
-//        } else {
-//            ""
-//        }
-//    }
-
     private fun performGameCollectionRequest(username: String): String {
         val apiUrl = "https://www.boardgamegeek.com/xmlapi2/collection?username=$username"
         val url = URL(apiUrl)
@@ -86,7 +60,6 @@ class MainActivity : AppCompatActivity() {
             reader.close()
 
             val apiResponse = response.toString()
-//            println("API Response: $apiResponse") // Print the API response for debugging
 
             apiResponse
         } else {
@@ -104,40 +77,8 @@ class MainActivity : AppCompatActivity() {
                 Toast.LENGTH_SHORT
             ).show()
         } else {
-            // User found, fetch and save the XML response
-            val gamesListUrl = "https://www.boardgamegeek.com/xmlapi2/collection?username=$username"
-            val apiRequest = URL(gamesListUrl)
-            val connection: HttpURLConnection = apiRequest.openConnection() as HttpURLConnection
-            connection.requestMethod = "GET"
-            val responseCode = connection.responseCode
-
-            if (responseCode == HttpURLConnection.HTTP_OK) {
-                val inputStream: InputStream = connection.inputStream
-                val file = File(filesDir, "game_list.xml") // Name and location of the XML file
-                val fileOutputStream = FileOutputStream(file)
-
-                inputStream.use { input ->
-                    fileOutputStream.use { output ->
-                        input.copyTo(output) // Copy the XML content to the file
-                    }
-                }
-
-                fileOutputStream.close()
-                inputStream.close()
-
                 val intent = Intent(this@MainActivity, UserView::class.java)
                 startActivity(intent)
-            } else {
-                // Handle the case when the response code is not HTTP_OK
-                Toast.makeText(
-                    this@MainActivity,
-                    "Error: Response code $responseCode",
-                    Toast.LENGTH_SHORT
-                ).show()
             }
         }
     }
-
-
-
-}
--- app/src/main/java/edu/put/and_test/UserView.kt
+++ app/src/main/java/edu/put/and_test/UserView.kt
@@ -1,11 +1,83 @@
 package edu.put.and_test
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TableLayout
+import android.widget.TableRow
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import java.io.File
 
 class UserView : AppCompatActivity() {
+
+    private lateinit var usernameTextView: TextView
+    private lateinit var numGamesTextView: TextView
+    private lateinit var numAddOnsTextView: TextView
+    private lateinit var lastSyncTextView: TextView
+    private lateinit var gamesButton: Button
+    private lateinit var addOnsButton: Button
+    private lateinit var syncButton: Button
+    private lateinit var clearDataButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_view)
+
+        usernameTextView = findViewById(R.id.usernameTextView)
+        numGamesTextView = findViewById(R.id.numGamesTextView)
+        numAddOnsTextView = findViewById(R.id.numAddOnsTextView)
+        lastSyncTextView = findViewById(R.id.lastSyncTextView)
+        gamesButton = findViewById(R.id.gamesButton)
+        addOnsButton = findViewById(R.id.addOnsButton)
+        syncButton = findViewById(R.id.syncButton)
+        clearDataButton = findViewById(R.id.clearDataButton)
+
+        // Retrieve user account data and display it
+        val username = "JohnDoe"
+        val numGames = 10
+        val numAddOns = 5
+        val lastSyncDate = "2023-06-08"
+
+        usernameTextView.text = "Username: $username"
+        numGamesTextView.text = "Number of Games: $numGames"
+        numAddOnsTextView.text = "Number of Add-Ons: $numAddOns"
+        lastSyncTextView.text = "Last Sync Date: $lastSyncDate"
+
+        // Set click listeners for buttons
+//        gamesButton.setOnClickListener {
+//            // Navigate to Games list activity
+//            startActivity(Intent(this, GamesListActivity::class.java))
+//        }
+//
+//        addOnsButton.setOnClickListener {
+//            // Navigate to Add-Ons list activity
+//            startActivity(Intent(this, AddOnsListActivity::class.java))
+//        }
+//
+//        syncButton.setOnClickListener {
+//            // Navigate to Sync activity
+//            startActivity(Intent(this, SyncActivity::class.java))
+//        }
+
+//        clearDataButton.setOnClickListener {
+//            // Show confirmation dialog for clearing data
+//            AlertDialog.Builder(this)
+//                .setTitle("Clear Data")
+//                .setMessage("Are you sure you want to clear all data?")
+//                .setPositiveButton("Yes") { _, _ ->
+//                    // Clear data and exit the application
+//                    clearData()
+//                    finish()
+//                }
+//                .setNegativeButton("No", null)
+//                .show()
+//        }
+    }
+
+    private fun clearData() {
+        // Clear data logic goes here
+        // Implement the necessary code to clear user data
     }
 }
