364e3d444bebff4ebbf3ec43b0f97cafd697fbf8
JakubWieclaw <jwieclaw000@gmail.com>
2023-06-10 19:15:51 +0200

    add minor fixes of display

--- app/src/main/java/edu/put/and_test/MainActivity.kt
+++ app/src/main/java/edu/put/and_test/MainActivity.kt
@@ -8,12 +8,18 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.TextView
 import android.widget.Toast
 import edu.put.and_test.models.User
 import edu.put.and_test.R
 import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var startButton: Button
+    private lateinit var progressBar: ProgressBar
+    private lateinit var progressText: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -35,11 +41,14 @@ class MainActivity : AppCompatActivity() {
             return
         }
 
-        val startButton: Button = findViewById(R.id.button_start)
+        startButton = findViewById(R.id.button_start)
+        progressBar = findViewById(R.id.progressBar)
+        progressText = findViewById(R.id.progressText)
 
         startButton.setOnClickListener {
             startButton.isEnabled = false
-            startButton.isClickable = false// Disable the button
+            startButton.isClickable = false // Disable the button
+            showProgressBar(true)
 
             val usernameEditText: EditText = findViewById(R.id.username)
             val username = usernameEditText.text.toString()
@@ -53,28 +62,39 @@ class MainActivity : AppCompatActivity() {
                 } catch (e: Exception) {
                     e.printStackTrace()
                 } finally {
-                    startButton.isEnabled = true  // Enable the button after the operation is completed
+                    startButton.isEnabled = true // Enable the button after the operation is completed
                     startButton.isClickable = true
+                    showProgressBar(false)
                 }
             }
         }
-
     }
 
-        // Handle the API response and take appropriate actions
-        private fun handleApiResponse(user: User?) {
-            if (user == null) {
-                // User not found, show a notification
-                Toast.makeText(
-                    this@MainActivity,
-                    "User not found, please try again.",
-                    Toast.LENGTH_SHORT
-                ).show()
-            } else {
-                // User found, navigate to UserView activity
-                val intent = Intent(this@MainActivity, UserView::class.java)
-                //intent.putExtra("user", user.username)
-                startActivity(intent)
-            }
+    // Handle the API response and take appropriate actions
+    private fun handleApiResponse(user: User?) {
+        if (user == null) {
+            // User not found, show a notification
+            Toast.makeText(
+                this@MainActivity,
+                "User not found, please try again.",
+                Toast.LENGTH_SHORT
+            ).show()
+        } else {
+            // User found, navigate to UserView activity
+            val intent = Intent(this@MainActivity, UserView::class.java)
+            //intent.putExtra("user", user.username)
+            startActivity(intent)
         }
     }
+
+    // Show or hide the progress bar
+    private fun showProgressBar(show: Boolean) {
+        if (show) {
+            progressBar.visibility = ProgressBar.VISIBLE
+            progressText.visibility = TextView.VISIBLE
+        } else {
+            progressBar.visibility = ProgressBar.GONE
+            progressText.visibility = TextView.GONE
+        }
+    }
+}
